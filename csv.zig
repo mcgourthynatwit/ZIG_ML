60d2f6240f296939ae02fbec232f84eda67fc9e0
nickmcgourthy <nickmcgourthy@gmail.com>
2024-10-16 11:42:43 -0400

    Redesigned read_csv, still taking some time (> 2.5 s for my test file with 40,000 rows) but adding multiprocessing & further optimizations should improve.

--- csv.zig
+++ csv.zig
@@ -1,114 +1,75 @@
 const std = @import("std");
 
+pub const TableError = error{ MissingHeader, OutOfMemory };
+
 pub const Table = struct {
-    pub const CsvType = union(enum) {
-        int: i64,
-        float: f64,
-        string: []const u8,
-        boolean: bool,
-    };
+    allocator: std.mem.Allocator,
+    arena_allocator: std.heap.ArenaAllocator,
+    headers: std.ArrayListAligned([]const u8, null),
+    body: std.ArrayListAligned([]const u8, null),
 
-    rows: usize,
-    cols: usize,
-    labels: []*u8,
-    data: [][]CsvType,
-
-    fn processHeader(allocator: std.mem.Allocator, header: *std.ArrayList([]u8), line: []const u8) !void {
-        var start: usize = 0;
-        for (line, 0..) |c, i| {
-            if (c == ',') {
-                try header.append(try allocator.dupe(u8, line[start..i]));
-                start = i + 1;
-            }
-        }
-        if (start < line.len) {
-            try header.append(try allocator.dupe(u8, line[start..]));
-        }
+    pub fn init(allocator: std.mem.Allocator) Table {
+        return Table{
+            .allocator = allocator,
+            .arena_allocator = std.heap.ArenaAllocator.init(allocator),
+            .headers = std.ArrayList([]const u8).init(allocator),
+            .body = std.ArrayList([]const u8).init(allocator),
+        };
     }
 
-    fn freeTable(allocator: std.mem.Allocator, lines: *std.ArrayList([]u8), header: *std.ArrayList([]u8)) void {
-        for (header.items) |item| {
-            allocator.free(item);
-        }
-        header.deinit();
-
-        for (lines.items) |item| {
-            allocator.free(item);
-        }
-        lines.deinit();
+    pub fn deinit(self: *Table) void {
+        self.headers.deinit();
+        self.body.deinit();
+        self.arena_allocator.deinit();
     }
 
-    fn readCsv(allocator: std.mem.Allocator, path: []const u8) !void {
-        const file = try std.fs.cwd().openFile(path, .{});
+    pub fn readCsv(allocator: std.mem.Allocator, file_name: []const u8) ![]const u8 {
+        const file = try std.fs.cwd().openFile(file_name, .{});
         defer file.close();
 
-        var buf_reader = std.io.bufferedReader(file.reader());
-        var in_stream = buf_reader.reader();
-        var buf: [1048576]u8 = undefined;
+        const file_size = try file.getEndPos();
 
-        // used when reading a line & buff reaches the end
-        var buf_offset: usize = 0;
+        const buffer = try allocator.alloc(u8, file_size);
+        errdefer allocator.free(buffer);
 
-        var total_lines: usize = 0;
-        var line_start: usize = 0;
+        const bytes_read = try file.readAll(buffer);
 
-        var header = std.ArrayList([]u8).init(allocator);
-        var lines = std.ArrayList([]u8).init(allocator);
+        std.debug.assert(bytes_read == file_size);
 
-        // free header & lines
-        defer freeTable(allocator, &lines, &header);
+        return buffer;
+    }
 
-        const start_time = std.time.milliTimestamp();
+    pub fn parse(self: *Table, csv_data: []const u8) TableError!void {
+        var rows = std.mem.split(u8, csv_data, "\n");
+        var header = std.mem.split(u8, rows.next() orelse return TableError.MissingHeader, ",");
+        var body = std.mem.split(u8, rows.rest(), ",");
 
-        while (true) {
-            const bytes_read = try in_stream.readAll(buf[buf_offset..]);
+        self.headers.clearAndFree();
+        self.body.clearAndFree();
 
-            if (bytes_read == 0) break;
-
-            const total_bytes_in_buffer = buf_offset + bytes_read;
-            var line_end: usize = buf_offset;
-
-            while (line_end < total_bytes_in_buffer) : (line_end += 1) {
-                if (buf[line_end] == '\n') {
-                    //const line = buf[line_start..line_end];
-
-                    // assume line 1 is headers
-                    if (total_lines == 0) {
-                        //try processHeader(allocator, &header, line);
-                    } else {
-                        // line
-                    }
-
-                    total_lines += 1;
-                    line_start = line_end + 1;
-                }
-            }
-            // if line_start is less then totalBytes, then the line is not finisehd processing
-            if (line_start < total_bytes_in_buffer) {
-                buf_offset = total_bytes_in_buffer - line_start;
-
-                for (0..buf_offset) |i| {
-                    buf[i] = buf[line_start + i];
-                }
-
-                line_start = 0;
-            }
-            // reset buffOffset if line fully processed
-            else {
-                buf_offset = 0;
-            }
-        }
-        const end_time = std.time.milliTimestamp();
-        const elapsed_milliseconds = end_time - start_time;
-        const elapsed_seconds = @as(f64, @floatFromInt(elapsed_milliseconds)) / 1000.0;
-        std.debug.print("Time taken: {d:.3} seconds\n", .{elapsed_seconds});
-        std.debug.print("Total lines processed: {d}\n", .{total_lines});
+        while (header.next()) |key| if (key.len > 0) try self.headers.append(key);
+        while (body.next()) |row| if (row.len > 0) try self.body.append(row);
     }
 };
 
 pub fn main() !void {
-    const path: []const u8 = "data/train.csv";
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-    try Table.readCsv(allocator, path);
+
+    const csv_data = try Table.readCsv(allocator, "data/train.csv");
+    defer allocator.free(csv_data);
+
+    var table: Table = Table.init(allocator);
+    defer table.deinit();
+
+    const start_time = std.time.milliTimestamp();
+
+    try table.parse(csv_data);
+
+    const end_time = std.time.milliTimestamp();
+    const elapsed_milliseconds = end_time - start_time;
+    const elapsed_seconds = @as(f64, @floatFromInt(elapsed_milliseconds)) / 1000.0;
+    std.debug.print("Time taken: {d:.3} seconds\n", .{elapsed_seconds});
+    std.debug.print("Total lines processed: {s}\n", .{table.body.items[0..table.headers.items.len]});
 }
