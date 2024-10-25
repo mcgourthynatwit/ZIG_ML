94af389e23231307ac41648a3115f829c5032d7d
nickmcgourthy <nickmcgourthy@gmail.com>
2024-10-25 11:12:03 -0400

    MSE, MAE, RMSE complete.

--- tensor.zig
+++ tensor.zig
@@ -6,8 +6,6 @@ pub const TensorError = error{ InvalidDimensions, OutOfBounds, NonInvertibleMatr
 
 pub const RegressionResult = struct {
     r_squared: f32,
-    p_values: []f32,
-    t_statistics: []f32,
     mse: f32,
     mae: f32,
     rmse: f32,
@@ -390,10 +388,61 @@ pub const Tensor = struct {
         return sum / n;
     }
 
-    //pub fn r2(Y_A: Tensor, Y_P: Tensor) !Tensor {}
+    // Calculates r^2 given Y_A -> actual y val & Y_P -> predicted y val
+    pub fn rSquared(Y_A: Tensor, Y_P: Tensor) f32 {
+        var SSR: f32 = 0.0;
+        var SST: f32 = 0.0;
+        const M: f32 = mean(Y_A);
+
+        for (0..Y_A.data.len) |i| {
+            SSR += (Y_A.data[i] - Y_P.data[i]) * (Y_A.data[i] - Y_P.data[i]);
+            SST += (Y_A.data[i] - M) * (Y_A.data[i] - M);
+        }
+
+        return (1 - (SSR / SST));
+    }
+
+    // Calculates MSE given Y_A -> actual y val & Y_P -> Predicted y val
+    pub fn mse(Y_A: Tensor, Y_P: Tensor) f32 {
+        var sum: f32 = 0.0;
+        const n: f32 = @as(f32, @floatFromInt(Y_A.data.len));
+
+        for (0..Y_A.data.len) |idx| {
+            const val: f32 = Y_A.data[idx] - Y_P.data[idx];
+            sum += (val * val);
+        }
+
+        return sum / n;
+    }
+
+    // Calculates RMSE given Y_A -> actual y val & Y_P -> Predicted y val
+    pub fn rmse(Y_A: Tensor, Y_P: Tensor) f32 {
+        var sum: f32 = 0.0;
+        const n: f32 = @as(f32, @floatFromInt(Y_A.data.len));
+
+        for (0..Y_A.data.len) |idx| {
+            const val: f32 = Y_A.data[idx] - Y_P.data[idx];
+            sum += (val * val);
+        }
+
+        return std.math.sqrt(sum / n);
+    }
+
+    // Calculates MAE given Y_A -> actual y val & Y_P -> Predicted y val
+    pub fn mae(Y_A: Tensor, Y_P: Tensor) f32 {
+        const n: f32 = @as(f32, @floatFromInt(Y_A.data.len));
+        var sum: f32 = 0.0;
+
+        for (0..Y_A.data.len) |idx| {
+            const val: f32 = Y_A.data[idx] - Y_P.data[idx];
+            sum += (val * val);
+        }
+
+        return @abs(sum / n);
+    }
 
     // OLS
-    pub fn linearRegression(X: Tensor, Y: Tensor) !Tensor {
+    pub fn linearRegression(X: Tensor, Y: Tensor) !RegressionResult {
         // Calculate beta = (X^T X)^-1 X^T Y
         var X_C: Tensor = try Tensor.initTensor(X.allocator, X.shape[0], X.shape[1], X.data);
 
@@ -419,15 +468,12 @@ pub const Tensor = struct {
         X_T.deinit();
         defer X_C.deinit();
 
-        // X_C is the result tensor, calculate results
-        //var r_squared: f32 = 0.0;
-        //var p_values: []f32 = {};
-        //var t_statistics: []f32 = {};
-        //var mse: f32 = 0.0;
-        //var mae: f32 = 0.0;
-        //var vrmse: f32 = 0.0;
-        //var predictions: []f32 = {};
-
-        return X_C;
+        return RegressionResult{
+            .r_squared = rSquared(Y, X_C),
+            .mse = mse(Y, X_C),
+            .mae = mae(Y, X_C),
+            .rmse = rmse(Y, X_C),
+            .predictions = X_C.data,
+        };
     }
 };
